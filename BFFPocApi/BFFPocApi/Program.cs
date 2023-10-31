3fbfded172261b45009d5c5b9cc469ea63bf2b99
Louis van Zyl <louisvanzyl@outlook.com>
2023-10-31 08:10:52 +0200

    Add base cookie setup

--- BFFPocApi/BFFPocApi/Program.cs
+++ BFFPocApi/BFFPocApi/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Authentication;
+using System.Net.Http.Headers;
+using Yarp.ReverseProxy.Transforms;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,16 +11,59 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddReverseProxy()
+                .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+                .AddTransforms(tCtx =>
+                {
+                    tCtx.AddRequestTransform(async rc =>
+                    {
+                        if (rc.DestinationPrefix == "https://www.googleapis.com")
+                        {
+                            var accessToken = await rc.HttpContext.GetTokenAsync("access_token");
+                        }
+                    });
+                });
+
+builder.Services.AddHttpClient()
+                .AddDataProtection();
+
 builder.Services.AddAuthentication("auth-cookie")
     .AddCookie("auth-cookie")
-    .AddOAuth("youtube", o => {
-        o.SignInScheme= "auth-cookie";
-        o.ClientId = "someClientID";
-        o.ClientSecret = "someClientSecret";
-        o.SaveTokens = true;
-});
+    .AddOAuth("youtube", auth =>
+    {
+        auth.SignInScheme = "auth-cookie";
+        auth.ClientId = "someClientID";
+        auth.ClientSecret = "someClientSecret";
+        auth.SaveTokens = true;
+
+        auth.Scope.Clear();
+        auth.Scope.Add("https://www.googleapis.com/auth/youtube.readonly");
+
+        auth.AuthorizationEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
+        auth.TokenEndpoint = "https://oauth.googleapis.com/token";
+        auth.CallbackPath = "/oauth/yt-cb";
+    });
 
 var app = builder.Build();
+
+app.MapGet("/login", () => Results.Challenge(new AuthenticationProperties()
+{
+    RedirectUri = "/"
+}, authenticationSchemes: new List<string>() { "youtube" }));
+
+app.MapGet("/api-yt", async (IHttpClientFactory clientFactory, HttpContext ctx) =>
+{
+    var accessToken = await ctx.GetTokenAsync("access_token");
+    var client = clientFactory.CreateClient();
+
+    Console.WriteLine(accessToken);
+
+
+    using var req = new HttpRequestMessage(HttpMethod.Get, "https://www.googleapis.com/youtube/v3/channels?part=snippet&mine=true");
+    req.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+    using var response = await client.SendAsync(req);
+    return await response.Content.ReadAsStringAsync();
+}).RequireAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
